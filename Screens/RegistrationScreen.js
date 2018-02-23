70cdef4f938084a4a7e4bc078f40eb87270c02f7
MUHAIMINUR RAHMAN <muhaiminurabir@gmail.com>
2018-02-23 17:05:01 +0600

    REGISTRATION PAGE INCLUDE

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -6,15 +6,31 @@ import {
   View,
   Button
 } from 'react-native';
+import LoginForm from './LoginForm';
+import RegistrationForm from './RegistrationForm';
 class RegistrationScreen extends Component{
 	render(){
 		return (
-			<View>
-			<Text>This is the REGISTRATION Screen</Text>
+			<View style={styles.container}>
+			<Text style={styles.welcome}>This is the REGISTRATION Screen</Text>
 			<Button onPress={() => this.props.navigation.navigate('HomeScreen')} title="HOME PAGE "/>
-			
+			<View style={styles.formContainer}>
+				<RegistrationForm/>
+			</View>
 			</View>
 			);
 	}
 }
+const styles = StyleSheet.create({
+	container:{
+		flex:1,
+		backgroundColor:'#3498db'
+
+	},
+	welcome:{
+		textAlign:'center',
+		color:'#FFF',
+		fontWeight:'700'
+	}
+});
 export default RegistrationScreen;
